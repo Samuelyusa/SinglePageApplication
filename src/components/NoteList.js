801610d4b09aa13f498920746321723c338548ff
Samuelyusa <samuelyusa@gmail.com>
2022-11-05 16:20:56 +0700

    Add desc when empty notes in NotesList

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -2,22 +2,22 @@ import React from 'react';
 import NoteItem from './NoteItem';
 
 function NoteList({ notes }) {
-    if (!notes.length) {
-        return () => {
+    if (notes.length === 0) {
+        return (
             <div className='notes-list-empty'>
                 <p>Tidak ada Catatan</p>
             </div>
-        }
-        
+        );
     }
-
-    return (
-        <div className='notes-list'>
-        {notes.map((note) => (
-            <NoteItem key={note.id} {...note} />
-        ))}
-        </div>
-    );
+    else {
+        return (
+            <div className='notes-list'>
+            {notes.map((note) => (
+                <NoteItem key={note.id} {...note} />
+            ))}
+            </div>
+        );
+    }
 }
 
 export default NoteList;
